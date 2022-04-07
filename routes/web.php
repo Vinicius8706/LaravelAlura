f78cb2d3ce606dcf650e3cbfa7cb9437189168cd
Vinicius <fsvinicius9@gmail.com>
2022-04-07 20:35:59 -0300

    criando o controllerSeries

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', function () {
-    $series = [
-        'Greys Anatomy', 'Lost' , 'Agents of SHIELD'
-    ];
-
-    $html = "<ul>";
-    foreach ($series as $serie){
-        $html .= "<li>$serie</li>";
-    };
-    $html .= "</ul>";
-
-    return $html;
-});
+Route::get('/series', 'SeriesController@listarSeries');
