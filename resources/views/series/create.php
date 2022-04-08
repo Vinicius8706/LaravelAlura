cc1d8875d035cd33859bd1f94906fbac6d640b71
Vinicius <fsvinicius9@gmail.com>
2022-04-08 11:49:00 -0300

    colocando bootstrap no sistema

--- resources/views/series/create.php
+++ resources/views/series/create.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <title>Controle de Séries</title>
+
+  <title>Document</title>
 </head>
 
 <body>
   <div class="container">
     <div class="jumbotron">
       <h1>
-        Séries
+        Adicionar série
       </h1>
     </div>
 
-    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
-    <ul class="list-group">
-      <?php foreach ($series as $series) : ?>
-        <li class="list-group-item"><?= $serie; ?>
-        <?php endforeach; ?>
-    </ul>
-  </div>
+    <form method='post'>
+      <label for="nome">Nome</label>
+      <div class="form-group">
+        <input type="text" class="form-control" name="nome" id="nome">
+        <button class="btn btn-primary">Adicionar</button>
+      </div>
+    </form>
+
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 
 Route::get('series',[SeriesController::class, 'index']);
+Route::get('series/criar',[SeriesController::class, 'create']);
