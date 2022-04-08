d44b605c32d3efb579187154de1bfae5591d6b00
Vinicius <fsvinicius9@gmail.com>
2022-04-08 12:16:49 -0300

    consertando series controller

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -5,7 +5,7 @@
  @section('conteudo')
      <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
      <ul class="list-group">
-         @foreach ($series as $series)
+         @foreach ($series as $serie)
              <li class="list-group-item"><?= $serie ?>
          @endforeach
      </ul>
