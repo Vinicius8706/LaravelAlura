84323f6b26e903aba9f95725b3faa49f7578b2c2
Vinicius <fsvinicius9@gmail.com>
2022-04-09 17:43:08 -0300

    Space between e justify content

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -12,12 +12,12 @@
      <a href={{route('form_criar_serie')}} class="btn btn-dark mb-2">Adicionar</a>
      <ul class="list-group">
          @foreach ($series as $serie)
-             <li class="list-group-item">{{$serie->nome}}
+             <li class="list-group-item d-flex justify-content-between align-items-center">{{$serie->nome}}
                  <form method="post" action="/series/remover/{{addslashes($serie->id)}}"
                  onsubmit="return confirm('Tem certeza?')">
                     @csrf
                      @method('DELETE')
-                     <button class="btn btn-danger">Excluir</button>
+                     <button class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></button>
                  </form></li>
 
          @endforeach
