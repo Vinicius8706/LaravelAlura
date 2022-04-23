67cf123c109d8fcb1de5da80d07e7e0d72efcb5e
Vinicius <fsvinicius9@gmail.com>
2022-04-23 14:42:36 -0300

    Colocando authentificadores

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\TemporadasController;
 
-Route::get('series', [SeriesController::class, 'index'])->name('listar_series');
-Route::get('series/criar', [SeriesController::class, 'create'])->name('form_criar_serie');
-Route::post('series/criar', [SeriesController::class, 'store']);
-Route::delete('/series/remover/{id}', [SeriesController::class, 'destroy']);
-Route::post('series/{id}/editaNome',[SeriesController::class, 'editaNome']);
-Route::get('/series/{serieId}/temporadas', [TemporadasController::class, 'index']);
-Route::get('temporadas/{temporada}/episodios',[EpisodiosController::class, 'index']);
-Route::post('temporadas/{temporada}/assistir',[EpisodiosController::class, 'assistir']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
